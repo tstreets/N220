00236f7f8a6bb9b6eea0323932aeaf3370a466cb
Ty Streets <tstreets@iu.edu>
2024-09-10 16:57:19 -0400

    Added window popup functions

--- week-3/app.js
+++ week-3/app.js
@@ -34,3 +34,77 @@ function sayMyName(myName) {
 sayMyName("Say my name");
 sayMyName("Say my name");
 sayMyName("If no one is around you");
+
+// Functions that do calculations
+
+function calcRectPerimeter(height, width) {
+  const perimeter = 2 * (height + width);
+  //   document.getElementById("panswer").innerHTML += perimeter;
+  document.querySelector("#panswer").innerHTML += perimeter;
+}
+
+calcRectPerimeter(2, 4);
+
+function calcArea() {
+  const height = document.getElementById("height").value;
+  const width = document.getElementById("width").value;
+  console.log("height", height);
+  console.log("width", width);
+  document.querySelector("#answer").innerHTML +=
+    "Area: " + height * width + "<br/>";
+  // `Area: ${height * width} <br />`
+}
+
+function calcRectAreaAndPeri(height, width) {
+  const perimeter = 2 * (height + width);
+  const area = height * width;
+
+  document.getElementById("multiAnswer").innerHTML += `
+  Height: ${height} <br />
+  Width: ${width} <br />
+  Perimeter: ${perimeter} <br />
+  Area: ${area} <br /> <br />
+  `;
+
+  document.getElementById("multiAnswer").innerHTML +=
+    "Height: " + height + "<br />";
+  document.getElementById("multiAnswer").innerHTML +=
+    "Width: " + width + "<br />";
+  document.getElementById("multiAnswer").innerHTML +=
+    "Perimeter: " + perimeter + "<br />";
+  document.getElementById("multiAnswer").innerHTML +=
+    "Area: " + area + "<br /> <br />";
+}
+
+calcRectAreaAndPeri(5, 4);
+calcRectAreaAndPeri(2, 8);
+
+function myFunction() {
+  alert("Who goes there?");
+}
+
+function wizard(name, occupation) {
+  // confirm("Welcome " + name + ", the " + occupation + "!");
+  confirm(`Welcome ${name}, the ${occupation}!`);
+  document.getElementById(
+    "future"
+  ).innerHTML += `Welcome ${name}, the ${occupation}!`;
+}
+
+function addItem() {
+  const newItem = prompt("Add Item:");
+  console.log(newItem);
+}
+
+function returnValue(color) {
+  return color;
+}
+
+alert(returnValue("green"));
+
+function calcCircleArea(radius) {
+  return 2 * Math.PI * radius;
+}
+
+document.getElementById("useOne").innerHTML += calcCircleArea(2);
+document.getElementById("useTwo").innerHTML += calcCircleArea(4);
