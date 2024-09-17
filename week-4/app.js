98b076fc1f547508b077e55a39804fe607684de0
Ty Streets <tstreets@iu.edu>
2024-09-17 16:44:30 -0400

    Extra array methods

--- week-4/app.js
+++ week-4/app.js
@@ -6,6 +6,8 @@ function printAnswer(answer) {
   answersPTag.innerHTML += `<br /> ${answer}`;
 }
 
+// ARRAY BASICS
+
 // 1. Arrays will print as a string seperated with commas by default
 answersPTag.innerHTML += studentGrades;
 
@@ -53,3 +55,54 @@ printAnswer(averageGrade);
 
 // 10.
 printAnswer(averageGrade > 80);
+
+// ARRAY MORE!!!!!!!!!
+
+// 0. Create an array
+const friends = [
+  "Elmo",
+  "Spongebob",
+  "CatDog",
+  "Danny Phantom",
+  "Timmy Turner",
+];
+
+function printFriends() {
+  document.querySelector("#extraAnswers").innerHTML += `<br /> ${friends}`;
+}
+
+// 1. Add items to an array using the push method
+friends.push("Jimmy Neutron");
+printFriends();
+
+// 2. Remove the last item to an array using the pop method
+friends.pop();
+printFriends();
+
+// 3. Remove the first item to an array using the slice method
+friends.splice(0, 1);
+printFriends();
+
+// 4. Add item to front of array using the
+friends.splice(0, 0, "Invader Zim");
+printFriends();
+
+// 5. Use splice again
+friends.splice(2, 1);
+printFriends();
+
+// 6. Splice
+friends.splice(2, 0, "Gir");
+printFriends();
+
+// 7. Splice
+friends.splice(3, 1, ["Patrick", "Squidward"]);
+printFriends();
+
+// splice method takes three parameters
+// 1. What index to start at
+// 2. How many elements starting from that index should be deleted
+// 3. What to add to the array
+//      This could be just a value or an array of values to add multiple
+
+// Objects
