b07194e5e9e7b9658829e1b0c617a1dd73002f71
Ty Streets <tstreets@iu.edu>
2024-11-12 15:41:11 -0500

    Adding boxes

---
+++ week-12/thebox.js
@@ -0,0 +1,30 @@
+const theBoxRef = document.getElementById("theBox");
+
+theBoxRef.style.border = "3px solid black";
+theBoxRef.style.minHeight = "300px";
+theBoxRef.style.display = "grid";
+theBoxRef.style.gridTemplateColumns = "1fr 1fr 1fr";
+
+function addToBox() {
+  const newThing = prompt("Thing to Add:");
+
+  const newDiv = document.createElement("div");
+  newDiv.style.height = "100px";
+  newDiv.style.width = "100px";
+  newDiv.style.backgroundColor = "lightgrey";
+  newDiv.style.margin = "5px";
+  newDiv.classList.add("box");
+  newDiv.innerHTML = newThing;
+
+  theBoxRef.appendChild(newDiv);
+
+  const allBoxes = document.querySelectorAll(".box");
+  for (let i = 0; i < allBoxes.length; i++) {
+    const box = allBoxes[i];
+    box.onclick = clickBox;
+  }
+}
+
+function clickBox(e) {
+  console.log(e.currentTarget);
+}
